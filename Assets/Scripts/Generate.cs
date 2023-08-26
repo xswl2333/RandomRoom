f8278066244430d11b93bee49323cdf558973424
xswl2333 <3120778195@qq.com>
2023-08-26 14:01:12 +0800

    :sparkles: 添加门优化boss房距离

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -2,50 +2,61 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Generate : MonoBehaviour
 {
-    public enum Direction { up,down,left,right};
-    public Direction direction; 
+    public enum Direction { up, down, left, right };
+    public Direction direction;
 
     [Header("房间预制体")]
     public GameObject roomPrefab;
     public int roomAmount;
     public Transform roomParent;//格子父物体,格子背后的image
-    public Vector3 roomPos=Vector3.zero;
+    public Vector3 roomPos = Vector3.zero;
     public GameObject endRoom;//最后一个房间，也是boos房
 
-    public float xOffset=2.0f;
-    public float yOffset=2.0f;
+    public float xOffset = 2.0f;
+    public float yOffset = 2.0f;
     public LayerMask roomLayer;
+    public int maxStep;
 
 
-    public List<GameObject> roomList=new List<GameObject>();
+    public List<Room> roomList = new List<Room>();
     // Start is called before the first frame update
+    public List<GameObject> farRooms = new List<GameObject>();
+    public List<GameObject> lessRooms = new List<GameObject>();
+    public List<GameObject> oneWayRooms = new List<GameObject>();
+
     void Start()
     {
         //BeginGenerate();
-        for(int i=0;i<roomAmount;i++)
+        for (int i = 0; i < roomAmount; i++)
         {
-            roomList.Add(Instantiate(roomPrefab, roomPos, Quaternion.identity));
+            roomList.Add(Instantiate(roomPrefab, roomPos, Quaternion.identity).GetComponent<Room>());
 
             //改变位置
             ChangeRoomPos();
         }
 
-        roomList[0].GetComponent<Room>().setRoomType(RoomType.Init);
+        roomList[0].setRoomType(RoomType.Init);
         //最远房间
-        endRoom = roomList[0];
-        foreach(var room in roomList)
+        endRoom = roomList[1].gameObject;
+        foreach (var room in roomList)
         {
-            if(room.transform.position.sqrMagnitude>endRoom.transform.position.sqrMagnitude)
-            {
-                endRoom=room;
-            }
+            //if (room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude)
+            //{
+            //    endRoom = room.gameObject;
+            //}
+
+            SetupRoom(room, room.transform.position);
 
         }
+
+        FindEndRoom();
+
         endRoom.GetComponent<Room>().setRoomType(RoomType.Boss);
 
     }
@@ -71,14 +82,66 @@ public class Generate : MonoBehaviour
                     roomPos += new Vector3(xOffset, 0, 0);
                     break;
             }
-        } while(Physics2D.OverlapCircle(roomPos, 0.2f, roomLayer));//碰撞盒的检测
+        } while (Physics2D.OverlapCircle(roomPos, 0.2f, roomLayer));//碰撞盒的检测
 
     }
+
+
+    public void SetupRoom(Room newRoom, Vector3 roomPosition)
+    {
+        newRoom.roomUp = Physics2D.OverlapCircle(roomPosition + new Vector3(0, yOffset, 0), 0.2f, roomLayer);
+        newRoom.roomDown = Physics2D.OverlapCircle(roomPosition + new Vector3(0, -yOffset, 0), 0.2f, roomLayer);
+        newRoom.roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0, 0), 0.2f, roomLayer);
+        newRoom.roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
+
+        newRoom.UpdateRoom();
+    }
+
+    public void FindEndRoom()
+    {
+        //最大数值 最远距离数字
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].stepToStart > maxStep)
+                maxStep = roomList[i].stepToStart;
+        }
+
+        //获得最远房间和第二远
+        foreach (var room in roomList)
+        {
+            if (room.stepToStart == maxStep)
+                farRooms.Add(room.gameObject);
+            if (room.stepToStart == maxStep - 1)
+                lessRooms.Add(room.gameObject);
+        }
+
+        for (int i = 0; i < farRooms.Count; i++)
+        {
+            if (farRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(farRooms[i]);//最远房间里的单侧门加入
+        }
+
+        for (int i = 0; i < lessRooms.Count; i++)
+        {
+            if (lessRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(lessRooms[i]);//第二远远房间里的单侧门加入
+        }
+
+        if (oneWayRooms.Count != 0)
+        {
+            endRoom = oneWayRooms[UnityEngine.Random.Range(0, oneWayRooms.Count)];
+        }
+        else
+        {
+            endRoom = farRooms[UnityEngine.Random.Range(0, farRooms.Count)];
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKey)
+        if (Input.anyKey)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
--- Assets/Scripts/Object/Room.cs
+++ Assets/Scripts/Object/Room.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Room : MonoBehaviour
 {
-   public int width, height;
-   public RoomType type;
+    public int width, height;
+    public RoomType type;
 
-   public void setRoomType(RoomType type)
-   {
-       this.type = type;
+    public GameObject doorLeft, doorRight, doorUp, doorDown;//用来存放各个位置的门
+
+    public bool roomLeft, roomRight, roomUp, roomDown;//判断上下左右是否有房间
+
+    public int stepToStart;//距离初始点的网格距离
+
+    public int doorNumber;//当前房间的门的数量/入口数量
+
+    void Start()
+    {
+        //对应方向的门是否显示，关联对应方向是否有其他房间
+        doorLeft.SetActive(roomLeft);
+        doorRight.SetActive(roomRight);
+        doorUp.SetActive(roomUp);
+        doorDown.SetActive(roomDown);
+    }
+
+    public void setRoomType(RoomType type)
+    {
+        this.type = type;
         switch (type)
         {
             case RoomType.Normal:
@@ -26,10 +44,26 @@ public class Room : MonoBehaviour
                 break;
         }
 
-   }
+    }
 
-   public RoomType getRoomType()
-   {
-       return this.type;
-   }
+    public RoomType getRoomType()
+    {
+        return this.type;
+    }
+
+    public void UpdateRoom()
+    {
+        //计算距离初始点的网格距离
+        stepToStart = (int)(Mathf.Abs(transform.position.x / 2) + Mathf.Abs(transform.position.y / 2));
+
+        if (roomUp)
+            doorNumber++;
+        if (roomDown)
+            doorNumber++;
+        if (roomLeft)
+            doorNumber++;
+        if (roomRight)
+            doorNumber++;
+    }
+
 }
