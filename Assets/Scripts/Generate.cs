4d220ee4eafc390c0915af9160b74cf0f47bc085
xswl2333 <3120778195@qq.com>
2023-08-27 19:11:04 +0800

    :recycle: 用单位圆来随机生成房间

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -3,34 +3,83 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class Generate : MonoBehaviour
 {
 
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+    public float xOffset = 2.0f;
+    public float yOffset = 2.0f;
+
     public GameObject roomPrefab;
     public Transform roomParent;//格子父物体,格子背后的image
-    // Start is called before the first frame update
+
+    public Vector3 roomPos = Vector3.zero;
+    public Vector3 roomSize = Vector3.zero;
+    public int roomAmount = 40;
+    public float radius = 2.0f;
+
+    public List<Room> roomList = new List<Room>();
+
     void Start()
     {
-        BeginGenerate();
+
+        for (int i = 0; i < roomAmount; i++)
+        {
+            var size = GetRandomPointInCircle(radius);
+            roomSize = new Vector3((float)size.x*2, (float)size.y*2, 0);
+            roomList.Add(Instantiate(roomPrefab, roomPos, Quaternion.identity).GetComponent<Room>());
+            roomList[i].setRoomSize(roomSize);
+            ChangeRoomPos();
+        }
     }
-    void BeginGenerate()
+
+    //单位圆随机生成大小
+    public Vector2 GetRandomPointInCircle(float radius)
     {
-        GameObject room1 = GameObject.Instantiate(roomPrefab, roomParent);
-        room1.transform.position= Vector3.zero;
-        room1.GetComponent<Room>().setRoomType(RoomType.Normal);
-        GameObject room2= GameObject.Instantiate(roomPrefab, roomParent);
-        room2.transform.position= new Vector3(3.0f,3.0f,0.0f);
-        room2.GetComponent<Room>().setRoomType(RoomType.Boss);
-        GameObject room3 = GameObject.Instantiate(roomPrefab, roomParent);
-        room3.transform.position = new Vector3(-3.0f, 3.0f, 0.0f);
-        room3.GetComponent<Room>().setRoomType(RoomType.Elite);
+        float angle = 2 * Mathf.PI * Random.value;//0-1之间随机
+        float len = Random.value + Random.value;
+        len = len < 1.0f ? len : 2 - len;//单位圆
+        return new Vector2(Mathf.Cos(angle) * len * radius, Mathf.Sin(angle) * len * radius);
+    }
+
+
+    public void ChangeRoomPos()
+    {
+        do
+        {
+            direction = (Direction)UnityEngine.Random.Range(0, 4);
+
+            switch (direction)
+            {
+                case Direction.up:
+                    roomPos += new Vector3(0, yOffset, 0);
+                    break;
+                case Direction.down:
+                    roomPos += new Vector3(0, -yOffset, 0);
+                    break;
+                case Direction.left:
+                    roomPos += new Vector3(-xOffset, 0, 0);
+                    break;
+                case Direction.right:
+                    roomPos += new Vector3(xOffset, 0, 0);
+                    break;
+            }
+        } while (Physics2D.OverlapCircle(roomPos, 1.0f));//碰撞盒的检测
 
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
--- Assets/Scripts/Object/Room.cs
+++ Assets/Scripts/Object/Room.cs
@@ -1,32 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
+using Color = UnityEngine.Color;
 
 public class Room : MonoBehaviour
 {
-   public int width, height;
-   public RoomType type;
+    [Header("房间的宽高")]
+    public Vector2 roomSize;
+    [Header("房间的类型")]
+    public RoomType type;
 
-   public void setRoomType(RoomType type)
-   {
-       this.type = type;
+    public void setRoomType(RoomType type)
+    {
+        this.type = type;
         switch (type)
         {
             case RoomType.Normal:
-            this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().color = new Color(170f / 255f, 120f / 255f, 110f / 255f);
-            break;
+                this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().color = new Color(170f / 255f, 120f / 255f, 110f / 255f);
+                break;
             case RoomType.Boss:
-            this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().color = new Color(124f / 255f, 56f / 255f, 65f / 255f);
-            break;
+                this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().color = new Color(124f / 255f, 56f / 255f, 65f / 255f);
+                break;
             case RoomType.Elite:
-            this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().color = new Color(90f / 255f, 90f / 255f, 190f / 255f);
-            break;
+                this.gameObject.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().color = new Color(90f / 255f, 90f / 255f, 190f / 255f);
+                break;
         }
 
-   }
+    }
 
-   public RoomType getRoomType()
-   {
-       return this.type;
-   }
+    public void setRoomSize(Vector2 size)
+    {
+        roomSize.x = size.x;
+        roomSize.y = size.y;
+        this.gameObject.transform.localScale = new Vector3(roomSize.x, roomSize.y, 0);//set没用，只有重新赋值
+    }
+    public RoomType getRoomType()
+    {
+        return this.type;
+    }
+
+    public Vector2 getRoomSize()
+    {
+        return new Vector2(roomSize.x, roomSize.y);
+    }
 }
